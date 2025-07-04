2e444680040df46023200b214b75a16763ad453b
Aveev Bair <aveevb123@gmail.com>
2025-07-04 13:09:24 +0300

    feat: implement login page with authentication component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import styles from './page.module.css';
-import Auth from '@/components/Auth';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Auth />
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
