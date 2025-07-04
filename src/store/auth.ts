81d9fadd7a5b9b867dcc9847fa6de56c04bf17aa
Aveev Bair <aveevb123@gmail.com>
2025-07-04 17:05:41 +0300

    feat: add persistence to authentication store for improved state retention

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,33 +1,41 @@
 import { AUTH_PATH } from '@/constants';
 import axios from 'axios';
+import { persist } from 'zustand/middleware';
 import { create } from 'zustand/react';
 
 import { AuthState } from '@/shared/types';
 
-const useAuthStore = create<AuthState>(set => ({
-  user: null,
-  isAuthenticated: false,
-  login: async authCredentials => {
-    try {
-      const response = await axios.post(
-        AUTH_PATH,
-        {
-          username: authCredentials.username,
-          password: authCredentials.password,
-          expiresInMins: 30
-        },
-        {
-          headers: { 'Content-Type': 'application/json' },
-          withCredentials: true
+const useAuthStore = create<AuthState>()(
+  persist(
+    set => ({
+      user: null,
+      isAuthenticated: false,
+      login: async authCredentials => {
+        try {
+          const response = await axios.post(
+            AUTH_PATH,
+            {
+              username: authCredentials.username,
+              password: authCredentials.password,
+              expiresInMins: 30
+            },
+            {
+              headers: { 'Content-Type': 'application/json' },
+              withCredentials: true
+            }
+          );
+          set({ user: response.data, isAuthenticated: true });
+        } catch (error) {
+          console.log(error, 'Ошибка авторизации в useAuthStore');
         }
-      );
-      set({ user: response.data, isAuthenticated: true });
-    } catch (error) {
-      console.log(error, 'Ошибка авторизации в useAuthStore');
+      },
+      logout: () => set({ user: null, isAuthenticated: false })
+    }),
+    {
+      name: 'auth-storage'
     }
-  },
-  logout: () => set({ user: null, isAuthenticated: false })
-}));
+  )
+);
 
 export const useIsAuthenticated = () =>
   useAuthStore(state => state.isAuthenticated);
